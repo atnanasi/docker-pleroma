1a3996ca4eb95e08d2549cdf59ca09877fed1339
Atnanasi <ys2000pro@gmail.com>
2020-04-18 17:06:09 +0900

    next

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,37 +1,23 @@
 use Mix.Config
 
 config :pleroma, Pleroma.Web.Endpoint,
-   url: [host: "pl.rokoucha.net", scheme: "https", port: 443],
+   url: [host: "pl-next.ggrel.net", scheme: "https", port: 443],
    http: [ip: {0, 0, 0, 0}, port: 4000]
 
 config :logger, level: :info
 
 config :pleroma, :instance,
-  name: "Pleroma/Rokoucha",
-  email: "admin+pleroma@rokoucha.net",
-  limit: 5000,
-  description: "Rokoucha's Pleroma instance",
+  name: "Pleroma Next",
+  email: "ys2000pro+next@gmail.com",
+  description: "Pleroma instance for a neophilia",
+  account_activation_required: true,
   finmoji_enabled: false,
-  registrations_open: false,
+  limit: 5000,
+  registrations_open: true,
   rewrite_policy: Pleroma.Web.ActivityPub.MRF.SimplePolicy
 
 config :pleroma, :mrf_simple,
-  reject: ["newjack.city", "mstdn.h3z.jp", "misskey.io"],
   media_removal: ["mstdn.jp"]
-
-config :pleroma, Pleroma.Upload,
-  uploader: Pleroma.Uploaders.S3,
-  strip_exif: false,
-  filters: [
-    Pleroma.Upload.Filter.AnonymizeFilename,
-    Pleroma.Upload.Filter.Mogrify
-  ]
-
-config :pleroma, Pleroma.Upload.Filter.Mogrify,
-  args: [
-    "strip",
-    "auto-orient"
-  ]
 
 config :pleroma, Oban,
   queues: [
